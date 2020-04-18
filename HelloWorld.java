17eb711b32db3d584479d3797b77424f7e3aefd7
LIUJIE-01 <liujilxy@gmail.com>
2020-04-18 20:01:37 +0800

    Add file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,5 @@
-/*
-	Java程序中最基本的组成单位.
-	类的定义格式:
-				public class 类名
-
-				}
-	这是我定义的HelloWorld类
-*/
-
 public class HelloWorld {
-	/*
-		这是main方法
-	
-		main方法时程序的入口方法,代码的执行是从main方法开始的
-	*/
-	public static void main(String[] args)  {
-		System.out.println("Hello World !");
-		//这是输出语句,("")里面的内容可以改变的
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
 	}
 }
